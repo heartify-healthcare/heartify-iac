e180e5cca35504abdf71930d7ca7fe90b5bac2e0
minhphuc2544 <phucle190904@gmail.com>
2025-12-11 09:48:56 +0700

    refactor: modularize eks iac

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,7 +1,29 @@
-output "cluster_endpoint" { 
-  value = module.eks.cluster_endpoint 
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
 }
 
-output "cluster_name" { 
-  value = module.eks.cluster_name 
+output "cluster_security_group_id" {
+  description = "Security group ids attached to the cluster control plane"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "node_security_group_id" {
+  description = "Security group id for the node groups"
+  value       = module.eks.node_security_group_id
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = module.eks.cluster_certificate_authority_data
+}
+
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = module.eks.cluster_name
+}
+
+output "oidc_provider_arn" {
+  description = "The ARN of the OIDC Provider"
+  value       = module.eks.oidc_provider_arn
 }
